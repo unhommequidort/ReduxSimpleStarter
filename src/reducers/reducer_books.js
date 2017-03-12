fdea04c42e772df96c120d6200aed3c7fd7a681f
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-03-12 14:39:23 -0500

    Add reducer_books reducer and book-list container component

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'The Moravian Night'},
+    { title: 'Malina'},
+    { title: 'Un ange cornu avec des ailes de tôle'},
+    { title: 'Green Henry'}
+  ];
+}
